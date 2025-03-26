f6516e021c4b82d509363952a088913686376b23
riley <riley@cnpbagwell.com>
2025-03-26 18:56:59 -0500

    First version of page

--- polls/templates/views.py
+++ polls/templates/views.py
@@ -15,3 +15,23 @@ def home(request):
 def test(request):
     template = loader.get_template("polls/test.html")
     return render(request, "polls/test.html")
+
+
+def about(request):
+    template = loader.get_template("polls/about-me.html")
+    return render(request, "polls/about-me.html")
+
+
+def projects(request):
+    template = loader.get_template("polls/projects.html")
+    return render(request, "polls/projects.html")
+
+
+def uses(request):
+    template = loader.get_template("polls/uses.html")
+    return render(request, "polls/uses.html")
+
+
+def contact(request):
+    template = loader.get_template("polls/contact.html")
+    return render(request, "polls/contact.html")
--- polls/urls.py
+++ polls/urls.py
@@ -5,5 +5,9 @@ from .templates import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("home", views.home, name="home"),
-    path("test", views.test, name="test")
+    path("test", views.test, name="test"),
+    path("about", views.about, name="about"),
+    path("projects", views.projects, name="projects"),
+    path("uses", views.uses, name="uses"),
+    path("contact", views.contact, name="contact"),
 ]
